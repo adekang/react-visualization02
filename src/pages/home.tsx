aca35db783185c195bdfebfa4e9583d240438fbb
TopLong <201497709@qq.com>
2021-04-28 21:39:07 +0800

    chart1 基本完成

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './home.scss';
 import headerBg from '../images/headerBg.png';
-
-console.log(headerBg);
+import {Chart1} from '../components/chart-1';
 
 export const Home = () => {
   return (
     <div className="home">
       <header style={{backgroundImage: `url(.${headerBg})`}}/>
       <main>
-        <section className="section1 bordered">1</section>
+        <section className="section1 bordered">
+          <Chart1/>
+          <Chart1/>
+          <Chart1/>
+        </section>
         <section className="section2">1</section>
         <section className="section3">1</section>
       </main>
