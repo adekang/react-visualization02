ac2c5955e66a49ab8bf2aa4feb02c70403a3d762
TopLong <201497709@qq.com>
2021-04-28 20:25:04 +0800

    添加 header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './home.scss';
+import headerBg from '../images/headerBg.png';
+
+console.log(headerBg);
 
 export const Home = () => {
   return (
-    <div>
-      <div className="x">
-        123
-      </div>
+    <div className="home">
+      <header style={{backgroundImage: `url(.${headerBg})`}}/>
     </div>
   );
 };
